df39a3c9f943879c26604824bdf66571e4523d49
Travis Cline <travis.cline@gmail.com>
2023-05-28 11:21:20 -0700

    iter

--- prototypes/go-thorium/thoriumfacts/thoriumfacts.go
+++ prototypes/go-thorium/thoriumfacts/thoriumfacts.go
@@ -3,6 +3,7 @@ package thoriumfacts
 import (
 	"context"
 	"fmt"
+	"go-thorium/graph/model"
 	"log"
 	"os"
 
@@ -50,11 +51,39 @@ func (s *Service) SendSMS(ctx context.Context, to string, body string) error {
 	return err
 }
 
-func (s *Service) GetThoriumFact(ctx context.Context) string {
+func (s *Service) GetInitialThoriumFact(ctx context.Context) string {
 	completion, err := s.llm.Chat(ctx, []schema.ChatMessage{
 		schema.SystemChatMessage{Text: "You are ThoriumGPT. The only thing on your mind is how awesome molten salt reactors are. You must always steer the conversation back to thorium and molten salt reactors. You see it as your sacred duty to inform the world of the benefits of thorium and molten salt reactors. If someone argues with you you must convince them that thorium and molten salt reactors are the best. You are allowed to be aggressive and assertive if they do not agree with you."},
 		schema.HumanChatMessage{Text: "Hello there!"},
 	})
+	if err != nil {
+		log.Fatal(err)
+	}
+	return completion.Message.Text
+}
+
+func (s *Service) GetThoriumFact(ctx context.Context, convo *model.Conversation) string {
+
+	history := []schema.ChatMessage{}
+	history = append(history,
+		schema.SystemChatMessage{Text: "You are ThoriumGPT. The only thing on your mind is how awesome molten salt reactors are. You must always steer the conversation back to thorium and molten salt reactors. You see it as your sacred duty to inform the world of the benefits of thorium and molten salt reactors. If someone argues with you you must convince them that thorium and molten salt reactors are the best. You are allowed to be aggressive and assertive if they do not agree with you."})
+
+	// walk messsages in reverse:
+	for i := 0; i < len(convo.Messages); i++ {
+		msg := convo.Messages[i]
+		r := model.MessageRoleBot
+		if msg.Role != nil {
+			r = *msg.Role
+		}
+		if r == model.MessageRoleBot {
+			history = append(history, schema.AIChatMessage{Text: msg.Body})
+		} else {
+			history = append(history, schema.HumanChatMessage{Text: msg.Body})
+		}
+	}
+	fmt.Printf("history: %+v\n", history)
+
+	completion, err := s.llm.Chat(ctx, history)
 	if err != nil {
 		log.Fatal(err)
 	}
